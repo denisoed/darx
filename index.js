1647f5fa8208ac86c4c55b47d911f61dd5b85308
denisoed <gufig93@mail.ru>
2017-08-15 12:44:31 +0300

    Add angular2

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
-  if(err) {
-    console.log('Could not connect to datbase: ', err);
-  }else {
-    console.log("Connect to database: ", config.db);
-  }
+    if (err) {
+        console.log('Could not connect to datbase: ', err);
+    } else {
+        console.log("Connect to database: ", config.db);
+    }
 });
 
+
+app.use(express.static(__dirname + '/client/dist/'))
+
 app.get('/', (req, res) => {
-  res.send('Hello men');
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
 app.listen(3000, () => {
-  console.log("Server working!");
+    console.log("Server working!");
 });
