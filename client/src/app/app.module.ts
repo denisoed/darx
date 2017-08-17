e2b3d9715671a721e65360c7463cad4278a8894a
denisoed <gufig93@mail.ru>
2017-08-17 08:59:59 +0300

    Add header component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    BlogComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
